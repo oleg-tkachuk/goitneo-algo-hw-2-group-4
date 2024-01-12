fde1a28d495fc31312140b11ca1a9c3748cda431
Oleg Tkachuk <oleg-tkachuk@users.noreply.github.com>
2024-01-13 01:42:08 +0200

    feat(request_processing.py): add homework 2

---
+++ request_processing.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import time
+import queue
+import random
+
+request_queue = queue.Queue()
+
+def generate_request():
+    request_id = random.randint(1000, 9999)
+    print(f"Homework 2 | Request ID={request_id} | Generating request")    
+    request_queue.put(request_id)
+    print(f"Homework 2 | Request ID={request_id} | Add a request to the queue")
+
+def process_request():
+    if not request_queue.empty():
+        request_id = request_queue.get()
+        print(f"Homework 2 | Request ID={request_id} | Request processing")
+    else:
+        print("Homework 2 | The queue is empty")
+
+def main():
+    while True:
+        generate_request()
+        process_request()
+        time.sleep(1)
+
+if __name__ == "__main__":
+    try:
+        print(f"Homework 2 | Starting processing requests")
+        main()
+    except KeyboardInterrupt:
+        print(f"\nHomework 2 | Good bye!")
+        try:
+            sys.exit(130)
+        except SystemExit:
+            os._exit(130)
